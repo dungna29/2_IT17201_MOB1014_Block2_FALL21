163b340220b181eb1660ab7b9e75062a589b3b46
Dungna89 <dungna29@fpt.edu.vn>
2021-11-01 10:45:01 +0700

    1

---
+++ 2_IT17201_MOB1014_BLOCK2_FALL21/src/BAI_1_LAMQUENVOIJAVA/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BAI_1_LAMQUENVOIJAVA;
+
+/**
+ *
+ * @author Dungna89
+ */
+public class NewClass {
+  public static void main(String[] args) {
+    System.out.println("Chào các bạn");
+  }
+}
